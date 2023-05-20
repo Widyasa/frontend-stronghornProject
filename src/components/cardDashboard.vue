6fe073f4fa856db8ad9a34eb72b4a4a0954a0f06
widya yasa <widyarusmananda15@gmail.com>
2023-05-20 20:48:40 +0800

    card dashboard

---
+++ src/components/cardDashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col">
+    <div class="card-dashboard-wrapper">
+      <div class="d-flex flex-row justify-content-between">
+        <div class="d-flex flex-column">
+          <p class="fw-semibold main-color">{{title_card}}</p>
+          <p class="title-font fs-3 mt-1">{{number_card}}</p>
+        </div>
+        <img :src="img_url" alt="" class="img-fluid" draggable="false">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cardDashboard",
+  props:{
+    title_card:{
+      type:String,
+      required:true
+    },
+    number_card:{
+      type:String,
+      required: true
+    },
+    img_url:{
+      type:String,
+      required:true
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/dashboardPage.vue
+++ src/views/dashboardPage.vue
@@ -1,10 +1,20 @@
 <template>
-  <p class="text-white">ini dashboard</p>
+  <div class="px-5 mt-5">
+      <div class="top-part row row-cols-xl-4 row-cols-lg-3 row-cols-sm-2 row-cols-1">
+        <card-dashboard :img_url="'../../public/img/dashboard/dashboard1.svg'" number_card="150" title_card="Number of Members" />
+        <card-dashboard :img_url="'../../public/img/dashboard/dashboard2.svg'" number_card="40" title_card="Number of Trainers" />
+        <card-dashboard :img_url="'../../public/img/dashboard/dashboard3.svg'" number_card="260" title_card="Number of Towels" />
+        <card-dashboard :img_url="'../../public/img/dashboard/dashboard1.svg'" number_card="260" title_card="Number of Lockers" />
+      </div>
+  </div>
 </template>
 
 <script>
+import CardDashboard from "@/components/cardDashboard.vue";
+
 export default {
-  name: "dashboardPage"
+  name: "dashboardPage",
+  components: {CardDashboard}
 }
 </script>
 
