3b9c0bed341fd69801bb7ce162879cdedd96bec1
widya yasa <widyarusmananda15@gmail.com>
2023-06-09 01:44:42 +0800

    Service table

--- src/views/pages/landingPage/service/serviceIndex.vue
+++ src/views/pages/landingPage/service/serviceIndex.vue
@@ -30,32 +30,36 @@
       </div>
     </div>
     <div class="d-flex flex-column mt-5">
-      <p class="title-font fs-3 text-uppercase"> Table</p>
-      <div class="mt-3">
+      <div class="d-flex flex-row justify-content-between">
+        <p class="title-font fs-3 text-uppercase">service Table</p>
+        <button class="btn btn-color">Add Service</button>
+      </div>
+      <div class="mt-4">
         <div class="card-dashboard-wrapper p-4">
           <table class="table" id="dataTable">
             <thead>
             <tr>
               <th>No</th>
-              <th>Hero Title</th>
+              <th>Service Name</th>
               <th>Description</th>
-              <th>Button 1</th>
-              <th>Button 2</th>
-              <th>Photo Selection</th>
+              <th>Icon Service</th>
+              <th>Button</th>
               <th></th>
             </tr>
             </thead>
             <tbody>
             <tr>
               <td>1</td>
-              <td>Improve your fitness level for the better!</td>
+              <td>Strength Training</td>
               <td style="max-width: 250px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tellus dui, rhoncrut efficitur ac, varius at ante. Suspendisse.</td>
+              <td><img src="../../../../../public/img/table/service-icon.svg" alt=""></td>
               <td>Start Now</td>
-              <td>Learn More</td>
-              <td><img src="../../../../../public/img/table/photo1.png" alt=""></td>
               <td class="d-flex flex-row gap-2">
                 <RouterLink :to="{name:'detailOrder'}">
                   <img src="../../../../../public/img/table/edit-icon.svg" draggable="false" class="icon-table">
+                </RouterLink>
+                <RouterLink :to="{name:'detailOrder'}">
+                  <img src="../../../../../public/img/table/delete-icon.svg" draggable="false" class="icon-table">
                 </RouterLink>
               </td>
             </tr>
